7cd717ea587bf2f4e394da4732f0ffc74e6f7343
PathonScript <pathonscript@gmail.com>
2022-03-18 21:54:11 +0700

    Styles a bit

--- pages/about.js
+++ pages/about.js
@@ -7,17 +7,17 @@ export default function about() {
                 <title>How to Website | About</title>
             </Head>
             <h1>
-                <h1 className="worker-title">Pat</h1>            
+                <h1 className="center">Pat</h1>            
             </h1>
             <h2 className="worker-description">- Worker</h2>
 
             <h1>
-                <h1 className="worker-title">Yindee</h1>
+                <h1 className="center">Yindee</h1>
             </h1>
             <h2 className="worker-description">- Manager?</h2>
 
             <h1>
-                <h1 className="worker-title">BM & Ryu</h1>
+                <h1 className="center">BM & Ryu</h1>
             </h1>
             <h2 className="worker-description">- Designer?</h2>
         </>
--- pages/index.js
+++ pages/index.js
@@ -17,24 +17,34 @@ export default function Home() {
       <Head>
         <title>How to Website | Home</title>
       </Head>
+      <div class="center">
+        <h1>A website is created with HTML, CSS, JavaScript</h1>
+        <hr />
 
-      <h1>A website is created with HTML, CSS, JavaScript</h1>
-      <h2>HTML</h2>
-      <p>HTML is the basic skeleton of what a website is, the order of what is. What text is what component.</p>
-      <div className="image-wrapper">
-        <Image className="image" src={HTMLCode} />
-        <Image className="image" src={HTMLPreview} />
+        <h2>HTML</h2>
+        <p>HTML is the basic skeleton of what a website is, the order of what is. What text is what component.</p>
+        <div className="image-wrapper">
+          <div class="image">
+            <Image src={HTMLCode} />
+          </div>
+          <Image className="image" src={HTMLPreview} />
+        </div>
+
+        <h2>CSS</h2>
+        <p>CSS is very simple, it beautifies the html.</p>
+        <div className="image-wrapper">
+          <div class="image">
+            <Image className="image" src={CSSCode} />
+          </div>
+          <Image className="image" src={CSSPreview} width={150.77777777778} height={118} />
+        </div>
+
+        <br />
+        <h2>JavaScript</h2>
+        <p>JavaScript makes everything function, a button when clicked to do what.</p>
+        <button className="demo-btn">No JavaScript</button>
+        <button className="demo-btn" onClick={Alert}>With JavaScript</button>
       </div>
-      <h2>CSS</h2>
-      <p>CSS is very simple, it beautifies the html.</p>
-      <div className="image-wrapper">
-        <Image className="image" src={CSSCode} />
-        <Image className="image" src={CSSPreview} width={150.77777777778} height={118} />
-      </div>
-      <h2>JavaScript</h2>
-      <p>JavaScript makes everything function, a button when clicked to do what.</p>
-      <button className="demo-btn">No JavaScript</button>
-      <button className="demo-btn" onClick={Alert}>With JavaScript</button>
     </>
     // YINDEE IS A STUPID SON ON A BITCH
   )
