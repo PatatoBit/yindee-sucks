73e2e7cab4ff3317b1ed49a1e204bc9cc86500db
PathonScript <pathonscript@gmail.com>
2022-03-31 21:40:34 +0700

    Secrets

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,10 @@ export default function about() {
                 <title>How to Website | About</title>
             </Head>
             <h1>
-                <h1 className="center">Yindee</h1>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                    <h1 className="center">Yindee</h1>
+
+                </a>
             </h1>
             <h2 className="worker-description">- Manager</h2>
 
@@ -17,7 +20,8 @@ export default function about() {
             <h2 className="worker-description">- Designer</h2>
 
             <h6>
-                <h6 className="center">Pat - Worker</h6>
+                <a href="https://www.youtube.com/watch?v=M0Z-f9HYTb8"><h6 className="center">Pat - Worker</h6></a>
+                
             </h6>
 
         </>
